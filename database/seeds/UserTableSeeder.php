924da95199f57bc2ddf35db82e42547817c24b11
Bernardo Issenguel <beissenguel12@gmail.com>
2021-01-14 10:52:00 +0000

    user seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Bernardo',
+            'surname' => 'Issenguel',
+            'email' => 'beissenguel12@gmail.com',
+            'phone' => '912456876',
+            'estado' => 1,
+            'password' => Hash::make('123')
+        ]);
+    }
+}
